ce37dc9c67fd88e72e4346e1a907a1950fd6ccba
Adrian <klimasa98@gmail.com>
2023-06-17 15:49:23 -0400

    add bars observer to burger-btn

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const nav = document.querySelector('.nav')
 const navBtn = document.querySelector('.burger-btn')
 const allNavItems = document.querySelectorAll('.nav__item')
+const navBtnBars = document.querySelector('.burger-btn__bars')
+const Allsections = document.querySelectorAll('.section')
+const footerYear = document.querySelector('.footer__year')
 
 const handleNav = () => {
   nav.classList.toggle('nav--active')
@@ -37,3 +40,26 @@ const deleteAnimation = () => {
   })
 }
 
+
+const handleObserver = () => {
+  const currentSection = window.scrollY;
+
+  Allsections.forEach(section => {
+    if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60){
+      navBtnBars.classList.add('black-bars-color')
+    }
+    else if(!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60){
+      navBtnBars.classList.remove('black-bars-color')
+    }
+  })
+}
+
+const HandleCurrentYear = () => {
+  const year = new Date().getFullYear()
+  footerYear.innerText = year
+}
+
+HandleCurrentYear();
+
+
+window.addEventListener('scroll', handleObserver)
